3e6a2474a06b240786604635aa505880804fd8c1
David Zhao <taipan.snake13@gmail.com>
2017-05-05 22:36:04 +1000

    added path sample for provenance proof trees

---
+++ samples/path/path.cpp
@@ -0,0 +1,339 @@
+#include "souffle/CompiledSouffle.h"
+
+namespace souffle {
+using namespace ram;
+class Sf_path : public SouffleProgram {
+private:
+static inline bool regex_wrapper(const char *pattern, const char *text) {
+   bool result = false; 
+   try { result = std::regex_match(text, std::regex(pattern)); } catch(...) { 
+     std::cerr << "warning: wrong pattern provided for match(\"" << pattern << "\",\"" << text << "\")\n";
+}
+   return result;
+}
+static inline std::string substr_wrapper(const char *str, size_t idx, size_t len) {
+   std::string sub_str, result; 
+   try { result = std::string(str).substr(idx,len); } catch(...) { 
+     std::cerr << "warning: wrong index position provided by substr(\"";
+     std::cerr << str << "\"," << idx << "," << len << ") functor.\n";
+   } return result;
+}
+public:
+SymbolTable symTable;
+// -- Table: edge
+ram::Relation<Auto,2>* rel_1_edge;
+souffle::RelationWrapper<0,ram::Relation<Auto,2>,Tuple<RamDomain,2>,2,true,false> wrapper_rel_1_edge;
+// -- Table: edge_new
+ram::Relation<Auto,1>* rel_2_edge_new;
+// -- Table: edge_output
+ram::Relation<Auto,3>* rel_3_edge_output;
+souffle::RelationWrapper<1,ram::Relation<Auto,3>,Tuple<RamDomain,3>,3,false,true> wrapper_rel_3_edge_output;
+// -- Table: path_new
+ram::Relation<Auto,1, ram::index<0>>* rel_4_path_new;
+// -- Table: @delta_path_new
+ram::Relation<Auto,1>* rel_5_delta_path_new;
+// -- Table: @new_path_new
+ram::Relation<Auto,1>* rel_6_new_path_new;
+// -- Table: path_new_0
+ram::Relation<Auto,2>* rel_7_path_new_0;
+souffle::RelationWrapper<2,ram::Relation<Auto,2>,Tuple<RamDomain,2>,2,false,true> wrapper_rel_7_path_new_0;
+// -- Table: path_new_1
+ram::Relation<Auto,3, ram::index<0,1,2>>* rel_8_path_new_1;
+souffle::RelationWrapper<3,ram::Relation<Auto,3, ram::index<0,1,2>>,Tuple<RamDomain,3>,3,false,true> wrapper_rel_8_path_new_1;
+// -- Table: @delta_path_new_1
+ram::Relation<Auto,3>* rel_9_delta_path_new_1;
+// -- Table: @new_path_new_1
+ram::Relation<Auto,3>* rel_10_new_path_new_1;
+// -- Table: path_output
+ram::Relation<Auto,3>* rel_11_path_output;
+souffle::RelationWrapper<4,ram::Relation<Auto,3>,Tuple<RamDomain,3>,3,false,true> wrapper_rel_11_path_output;
+public:
+Sf_path() : 
+rel_1_edge(new ram::Relation<Auto,2>()),
+wrapper_rel_1_edge(*rel_1_edge,symTable,"edge",std::array<const char *,2>{"s:symbol","s:symbol"},std::array<const char *,2>{"x","y"}),
+rel_2_edge_new(new ram::Relation<Auto,1>()),
+rel_3_edge_output(new ram::Relation<Auto,3>()),
+wrapper_rel_3_edge_output(*rel_3_edge_output,symTable,"edge_output",std::array<const char *,3>{"r:edge_type{x#s:symbol,y#s:symbol}","s:symbol","s:symbol"},std::array<const char *,3>{"x","x_0","x_1"}),
+rel_4_path_new(new ram::Relation<Auto,1, ram::index<0>>()),
+rel_5_delta_path_new(new ram::Relation<Auto,1>()),
+rel_6_new_path_new(new ram::Relation<Auto,1>()),
+rel_7_path_new_0(new ram::Relation<Auto,2>()),
+wrapper_rel_7_path_new_0(*rel_7_path_new_0,symTable,"path_new_0",std::array<const char *,2>{"r:path_type{x#s:symbol,y#s:symbol}","r:edge_type{x#s:symbol,y#s:symbol}"},std::array<const char *,2>{"0","1"}),
+rel_8_path_new_1(new ram::Relation<Auto,3, ram::index<0,1,2>>()),
+wrapper_rel_8_path_new_1(*rel_8_path_new_1,symTable,"path_new_1",std::array<const char *,3>{"r:path_type{x#s:symbol,y#s:symbol}","r:edge_type{x#s:symbol,y#s:symbol}","r:path_type{x#s:symbol,y#s:symbol}"},std::array<const char *,3>{"0","1","2"}),
+rel_9_delta_path_new_1(new ram::Relation<Auto,3>()),
+rel_10_new_path_new_1(new ram::Relation<Auto,3>()),
+rel_11_path_output(new ram::Relation<Auto,3>()),
+wrapper_rel_11_path_output(*rel_11_path_output,symTable,"path_output",std::array<const char *,3>{"r:path_type{x#s:symbol,y#s:symbol}","s:symbol","s:symbol"},std::array<const char *,3>{"x","x_0","x_1"}){
+addRelation("edge",&wrapper_rel_1_edge,1,0);
+addRelation("edge_output",&wrapper_rel_3_edge_output,0,1);
+addRelation("path_new_0",&wrapper_rel_7_path_new_0,0,1);
+addRelation("path_new_1",&wrapper_rel_8_path_new_1,0,1);
+addRelation("path_output",&wrapper_rel_11_path_output,0,1);
+}
+~Sf_path() {
+delete rel_1_edge;
+delete rel_2_edge_new;
+delete rel_3_edge_output;
+delete rel_4_path_new;
+delete rel_5_delta_path_new;
+delete rel_6_new_path_new;
+delete rel_7_path_new_0;
+delete rel_8_path_new_1;
+delete rel_9_delta_path_new_1;
+delete rel_10_new_path_new_1;
+delete rel_11_path_output;
+}
+void run() {
+// -- initialize counter --
+std::atomic<RamDomain> ctr(0);
+
+#if defined(__EMBEDDED_SOUFFLE__) && defined(_OPENMP)
+omp_set_num_threads(1);
+#endif
+
+// -- query evaluation --
+if (!rel_1_edge->empty()) {
+auto part = rel_1_edge->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_1_edge_op_ctxt,rel_1_edge->createContext());
+CREATE_OP_CONTEXT(rel_2_edge_new_op_ctxt,rel_2_edge_new->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+Tuple<RamDomain,1> tuple({(RamDomain)(pack(ram::Tuple<RamDomain,2>({env0[0],env0[1]})))});
+rel_2_edge_new->insert(tuple,READ_OP_CONTEXT(rel_2_edge_new_op_ctxt));
+}
+PARALLEL_END;
+}
+if (!rel_2_edge_new->empty()) {
+auto part = rel_2_edge_new->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_2_edge_new_op_ctxt,rel_2_edge_new->createContext());
+CREATE_OP_CONTEXT(rel_3_edge_output_op_ctxt,rel_3_edge_output->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+auto ref = env0[0];
+if (isNull<ram::Tuple<RamDomain,2>>(ref)) continue;
+ram::Tuple<RamDomain,2> env1 = unpack<ram::Tuple<RamDomain,2>>(ref);
+{
+Tuple<RamDomain,3> tuple({(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[0],env1[1]}))),(RamDomain)(env1[0]),(RamDomain)(env1[1])});
+rel_3_edge_output->insert(tuple,READ_OP_CONTEXT(rel_3_edge_output_op_ctxt));
+}
+}
+PARALLEL_END;
+}
+if (!rel_2_edge_new->empty()) {
+auto part = rel_2_edge_new->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_2_edge_new_op_ctxt,rel_2_edge_new->createContext());
+CREATE_OP_CONTEXT(rel_7_path_new_0_op_ctxt,rel_7_path_new_0->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+auto ref = env0[0];
+if (isNull<ram::Tuple<RamDomain,2>>(ref)) continue;
+ram::Tuple<RamDomain,2> env1 = unpack<ram::Tuple<RamDomain,2>>(ref);
+{
+Tuple<RamDomain,2> tuple({(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[0],env1[1]}))),(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[0],env1[1]})))});
+rel_7_path_new_0->insert(tuple,READ_OP_CONTEXT(rel_7_path_new_0_op_ctxt));
+}
+}
+PARALLEL_END;
+}
+rel_9_delta_path_new_1->insertAll(*rel_8_path_new_1);
+if (!rel_7_path_new_0->empty()) {
+auto part = rel_7_path_new_0->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_4_path_new_op_ctxt,rel_4_path_new->createContext());
+CREATE_OP_CONTEXT(rel_7_path_new_0_op_ctxt,rel_7_path_new_0->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+Tuple<RamDomain,1> tuple({(RamDomain)(env0[0])});
+rel_4_path_new->insert(tuple,READ_OP_CONTEXT(rel_4_path_new_op_ctxt));
+}
+PARALLEL_END;
+}
+rel_5_delta_path_new->insertAll(*rel_4_path_new);
+for(;;) {
+SECTIONS_START;
+SECTION_START;
+if (!rel_5_delta_path_new->empty()&&!rel_2_edge_new->empty()) {
+auto part = rel_2_edge_new->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_5_delta_path_new_op_ctxt,rel_5_delta_path_new->createContext());
+CREATE_OP_CONTEXT(rel_10_new_path_new_1_op_ctxt,rel_10_new_path_new_1->createContext());
+CREATE_OP_CONTEXT(rel_2_edge_new_op_ctxt,rel_2_edge_new->createContext());
+CREATE_OP_CONTEXT(rel_8_path_new_1_op_ctxt,rel_8_path_new_1->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+auto ref = env0[0];
+if (isNull<ram::Tuple<RamDomain,2>>(ref)) continue;
+ram::Tuple<RamDomain,2> env1 = unpack<ram::Tuple<RamDomain,2>>(ref);
+{
+for(const auto& env2 : *rel_5_delta_path_new) {
+auto ref = env2[0];
+if (isNull<ram::Tuple<RamDomain,2>>(ref)) continue;
+ram::Tuple<RamDomain,2> env3 = unpack<ram::Tuple<RamDomain,2>>(ref);
+{
+if( ((((env1[1]) == (env3[0]))) && (!rel_8_path_new_1->contains(Tuple<RamDomain,3>({pack(ram::Tuple<RamDomain,2>({env1[0],env3[1]})),pack(ram::Tuple<RamDomain,2>({env1[0],env1[1]})),pack(ram::Tuple<RamDomain,2>({env1[1],env3[1]}))}),READ_OP_CONTEXT(rel_8_path_new_1_op_ctxt))))) {
+Tuple<RamDomain,3> tuple({(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[0],env3[1]}))),(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[0],env1[1]}))),(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[1],env3[1]})))});
+rel_10_new_path_new_1->insert(tuple,READ_OP_CONTEXT(rel_10_new_path_new_1_op_ctxt));
+}
+}
+}
+}
+}
+PARALLEL_END;
+}
+SECTION_END
+SECTION_START;
+if (!rel_9_delta_path_new_1->empty()) {
+auto part = rel_9_delta_path_new_1->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_9_delta_path_new_1_op_ctxt,rel_9_delta_path_new_1->createContext());
+CREATE_OP_CONTEXT(rel_6_new_path_new_op_ctxt,rel_6_new_path_new->createContext());
+CREATE_OP_CONTEXT(rel_4_path_new_op_ctxt,rel_4_path_new->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+if( !rel_4_path_new->contains(Tuple<RamDomain,1>({env0[0]}),READ_OP_CONTEXT(rel_4_path_new_op_ctxt))) {
+Tuple<RamDomain,1> tuple({(RamDomain)(env0[0])});
+rel_6_new_path_new->insert(tuple,READ_OP_CONTEXT(rel_6_new_path_new_op_ctxt));
+}
+}
+PARALLEL_END;
+}
+SECTION_END
+SECTIONS_END;
+if(((rel_10_new_path_new_1->empty()) && (rel_6_new_path_new->empty()))) break;
+rel_8_path_new_1->insertAll(*rel_10_new_path_new_1);
+{
+auto rel_0 = rel_9_delta_path_new_1;
+rel_9_delta_path_new_1 = rel_10_new_path_new_1;
+rel_10_new_path_new_1 = rel_0;
+}
+rel_10_new_path_new_1->purge();
+rel_4_path_new->insertAll(*rel_6_new_path_new);
+{
+auto rel_0 = rel_5_delta_path_new;
+rel_5_delta_path_new = rel_6_new_path_new;
+rel_6_new_path_new = rel_0;
+}
+rel_6_new_path_new->purge();
+}
+rel_9_delta_path_new_1->purge();
+rel_10_new_path_new_1->purge();
+rel_5_delta_path_new->purge();
+rel_6_new_path_new->purge();
+if (!rel_4_path_new->empty()) {
+auto part = rel_4_path_new->partition();
+PARALLEL_START;
+CREATE_OP_CONTEXT(rel_4_path_new_op_ctxt,rel_4_path_new->createContext());
+CREATE_OP_CONTEXT(rel_11_path_output_op_ctxt,rel_11_path_output->createContext());
+pfor(auto it = part.begin(); it<part.end(); ++it) 
+for(const auto& env0 : *it) {
+auto ref = env0[0];
+if (isNull<ram::Tuple<RamDomain,2>>(ref)) continue;
+ram::Tuple<RamDomain,2> env1 = unpack<ram::Tuple<RamDomain,2>>(ref);
+{
+Tuple<RamDomain,3> tuple({(RamDomain)(pack(ram::Tuple<RamDomain,2>({env1[0],env1[1]}))),(RamDomain)(env1[0]),(RamDomain)(env1[1])});
+rel_11_path_output->insert(tuple,READ_OP_CONTEXT(rel_11_path_output_op_ctxt));
+}
+}
+PARALLEL_END;
+}
+}
+public:
+void printAll(std::string dirname) {
+try {std::map<std::string, std::string> directiveMap({{"IO","stdout"},{"name","edge_output"}});
+if (!dirname.empty() && directiveMap["IO"] == "file" && directiveMap["filename"].front() != '/') {directiveMap["filename"] = dirname + "/" + directiveMap["filename"];}IODirectives ioDirectives(directiveMap);
+IOSystem::getInstance().getWriter(SymbolMask({0, 1, 1}), symTable, ioDirectives)->writeAll(*rel_3_edge_output);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+try {std::map<std::string, std::string> directiveMap({{"IO","stdout"},{"name","path_new_0"}});
+if (!dirname.empty() && directiveMap["IO"] == "file" && directiveMap["filename"].front() != '/') {directiveMap["filename"] = dirname + "/" + directiveMap["filename"];}IODirectives ioDirectives(directiveMap);
+IOSystem::getInstance().getWriter(SymbolMask({0, 0}), symTable, ioDirectives)->writeAll(*rel_7_path_new_0);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+try {std::map<std::string, std::string> directiveMap({{"IO","stdout"},{"name","path_new_1"}});
+if (!dirname.empty() && directiveMap["IO"] == "file" && directiveMap["filename"].front() != '/') {directiveMap["filename"] = dirname + "/" + directiveMap["filename"];}IODirectives ioDirectives(directiveMap);
+IOSystem::getInstance().getWriter(SymbolMask({0, 0, 0}), symTable, ioDirectives)->writeAll(*rel_8_path_new_1);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+try {std::map<std::string, std::string> directiveMap({{"IO","stdout"},{"name","path_output"}});
+if (!dirname.empty() && directiveMap["IO"] == "file" && directiveMap["filename"].front() != '/') {directiveMap["filename"] = dirname + "/" + directiveMap["filename"];}IODirectives ioDirectives(directiveMap);
+IOSystem::getInstance().getWriter(SymbolMask({0, 1, 1}), symTable, ioDirectives)->writeAll(*rel_11_path_output);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+}
+public:
+void loadAll(std::string dirname) {
+try {std::map<std::string, std::string> directiveMap({{"IO","file"},{"filename","./edge.facts"},{"name","edge"}});
+if (!dirname.empty() && directiveMap["IO"] == "file" && directiveMap["filename"].front() != '/') {directiveMap["filename"] = dirname + "/" + directiveMap["filename"];}IODirectives ioDirectives(directiveMap);
+IOSystem::getInstance().getReader(SymbolMask({1, 1}), symTable, ioDirectives)->readAll(*rel_1_edge);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+}
+public:
+void dumpInputs(std::ostream& out = std::cout) {
+try {IODirectives ioDirectives;
+ioDirectives.setIOType("stdout");
+ioDirectives.setRelationName("rel_1_edge");
+IOSystem::getInstance().getWriter(SymbolMask({1, 1}), symTable, ioDirectives)->writeAll(*rel_1_edge);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+}
+public:
+void dumpOutputs(std::ostream& out = std::cout) {
+try {IODirectives ioDirectives;
+ioDirectives.setIOType("stdout");
+ioDirectives.setRelationName("rel_3_edge_output");
+IOSystem::getInstance().getWriter(SymbolMask({0, 1, 1}), symTable, ioDirectives)->writeAll(*rel_3_edge_output);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+try {IODirectives ioDirectives;
+ioDirectives.setIOType("stdout");
+ioDirectives.setRelationName("rel_7_path_new_0");
+IOSystem::getInstance().getWriter(SymbolMask({0, 0}), symTable, ioDirectives)->writeAll(*rel_7_path_new_0);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+try {IODirectives ioDirectives;
+ioDirectives.setIOType("stdout");
+ioDirectives.setRelationName("rel_8_path_new_1");
+IOSystem::getInstance().getWriter(SymbolMask({0, 0, 0}), symTable, ioDirectives)->writeAll(*rel_8_path_new_1);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+try {IODirectives ioDirectives;
+ioDirectives.setIOType("stdout");
+ioDirectives.setRelationName("rel_11_path_output");
+IOSystem::getInstance().getWriter(SymbolMask({0, 1, 1}), symTable, ioDirectives)->writeAll(*rel_11_path_output);
+} catch (std::exception& e) {std::cerr << e.what();exit(1);}
+}
+public:
+const SymbolTable &getSymbolTable() const {
+return symTable;
+}
+};
+SouffleProgram *newInstance_path(){return new Sf_path;}
+SymbolTable *getST_path(SouffleProgram *p){return &reinterpret_cast<Sf_path*>(p)->symTable;}
+#ifdef __EMBEDDED_SOUFFLE__
+class factory_Sf_path: public souffle::ProgramFactory {
+SouffleProgram *newInstance() {
+return new Sf_path();
+};
+public:
+factory_Sf_path() : ProgramFactory("path"){}
+};
+static factory_Sf_path __factory_Sf_path_instance;
+}
+#else
+}
+int main(int argc, char** argv)
+{
+souffle::CmdOptions opt(R"(path.dl)",
+R"(.)",
+R"(.)",
+false,
+R"()",
+1
+);
+if (!opt.parse(argc,argv)) return 1;
+#if defined(_OPENMP) 
+omp_set_nested(true);
+#endif
+souffle::Sf_path obj;
+obj.loadAll(opt.getInputFileDir());
+obj.run();
+obj.printAll(opt.getOutputFileDir());
+return 0;
+}
+#endif
---
+++ samples/path/render_tree.h
@@ -0,0 +1,206 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include "assert.h"
+
+/***
+ * Class for keeping a rendered proof tree
+ */ 
+
+class screen_buffer {
+
+  private:
+
+    int width;          // width of the screen buffer
+    int height;         // height of the screen buffer
+    char *buffer; // screen contents
+
+  public:
+
+    // constructor
+    screen_buffer(int w, int h) : width(w), height(h), buffer(nullptr) { 
+      assert(width > 0 && height > 0 && "wrong dimensions"); 
+      buffer = new char[width * height]; 
+      memset(buffer, ' ', width * height);
+    } 
+
+    ~screen_buffer() { 
+      delete [] buffer;
+    }
+
+    // write into screen buffer at a specific location
+    void write(int x, int y, const std::string &s) {
+      assert(x >= 0 && x < width && "wrong x dimension");
+      assert(y >= 0 && y < height && "wrong y dimension"); 
+      assert(x + s.length() <= width && "string too long"); 
+      for(int i = 0;i < s.length(); i++) { 
+          buffer[y * width + x + i ] = s[i]; 
+      }
+    }
+
+    // print screen buffer
+    void print(std::ostream &os) { 
+      if (height > 0 && width > 0) {
+        for(int i=height-1;i>=0;i--) { 
+            for(int j=0;j<width;j++) {
+               os << buffer[width * i + j]; 
+            }
+            os << std::endl;
+        } 
+      } 
+    } 
+};
+
+/***
+ * Abstract Class for a Proof Tree Node
+ *  
+ */
+class tree_node {
+
+  protected:
+
+    std::string txt; // text of tree node
+    int width;       // width of node (including sub-trees)
+    int height;      // height of node (including sub-trees)
+    int xpos;        // x-position of text 
+    int ypos;        // y-position of text 
+
+  public:
+
+    tree_node(const std::string &t="") : txt(t), width(0), height(0), xpos(0), ypos(0) { 
+    }
+    virtual ~tree_node() { }
+
+    // get width
+    int getWidth() {
+      return width;
+    }
+
+    // get height
+    int getHeight() {
+      return height;
+    } 
+
+    // place the node
+    virtual void place(int xpos, int ypos) = 0;
+
+    // render node in screen buffer
+    virtual void render(screen_buffer &s) = 0; 
+};
+
+/***
+ * Concrete class
+ */ 
+class inner_node : public tree_node {
+
+  private: 
+
+    std::vector<std::unique_ptr<tree_node>> children;
+    std::string label;
+
+  public:
+
+    inner_node(const std::string &t="", const std::string &l="") : tree_node(t), label(l) {
+    }
+
+    // add child to node
+    void add_child(std::unique_ptr<tree_node> child) { 
+      children.push_back(std::move(child));
+    }
+
+    // place node and its sub-trees
+    void place(int x, int y) {
+      // there must exist at least one kid 
+      assert(children.size() > 0 && "no children"); 
+
+      // set x/y pos
+      xpos = x;
+      ypos = y; 
+
+      height = 0;
+      // compute size of bounding box 
+      //
+      for(const std::unique_ptr<tree_node> &k: children) { 
+        k->place(x, y + 2);
+        x += k->getWidth() + 1;
+        width += k->getWidth() + 1;
+        height = std::max(height, k->getHeight());
+      } 
+      width += label.length();
+      height += 2;
+  
+      // text of inner node is longer than all its sub-trees
+      if (width < txt.length()) {
+        width = txt.length();
+      }   
+    };
+
+    // render node text and separator line
+    void render(screen_buffer &s) {
+      s.write(xpos+(width - txt.length())/2, ypos, txt); 
+      for(const std::unique_ptr<tree_node> &k: children) { 
+        k->render(s);
+      }
+      std::string separator(width - label.length(),'-'); 
+      separator += label;
+      s.write(xpos,ypos+1,separator); 
+    } 
+};
+
+/***
+ * Concrete class for leafs 
+ */ 
+
+class leaf_node : public tree_node {
+  public:
+    leaf_node(const std::string &t="") : tree_node(t) {
+    }
+
+    // place leaf node
+    void place(int x, int y) { 
+      xpos = x; 
+      ypos = y; 
+      width = txt.length(); 
+      height = 1;  
+    } 
+
+    // render text of leaf node 
+    void render(screen_buffer &s) {
+      s.write(xpos,ypos,txt);  
+    }
+};
+
+/*
+int main()
+{
+
+  // create simple tree example
+  std::unique_ptr<leaf_node> l1 (new leaf_node("l1"));
+  std::unique_ptr<leaf_node> l2 (new leaf_node("l2"));
+  std::unique_ptr<inner_node> i1 (new inner_node("i1"));
+  i1->add_child(std::move(l1));
+  i1->add_child(std::move(l2));
+  std::unique_ptr<leaf_node> l3 (new leaf_node("1234567890l3"));
+  std::unique_ptr<leaf_node> l4 (new leaf_node("l4"));
+  std::unique_ptr<inner_node> i2 (new inner_node("i2"));
+  i2->add_child(std::move(l3));
+  i2->add_child(std::move(l4));
+  std::unique_ptr<inner_node> root (new inner_node("root"));
+  root->add_child(std::move(i1));
+  root->add_child(std::move(i2));
+
+  // Display tree
+
+  // compute bounding box and place nodes 
+  root->place(0,0); 
+
+  // create screen buffer
+  screen_buffer s(root->getWidth(), root->getHeight()); 
+
+  // render nodes
+  root->render(s);
+
+  // print tree
+  s.print(std::cout);
+} */
