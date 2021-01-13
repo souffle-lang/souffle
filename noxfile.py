21b6182762abe540865c9cf5146b996a962f1336
Tomas Puverle <puverle+github@gmail.com>
2021-01-12 20:36:55 -0800

    Improved noxfile and tool detection for Python.

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,18 @@
+import pathlib
+import sys
+
 import nox
+
+# This is a hack to extract autoconf paths into python
+sys.path.append("src")
+from python_paths import POETRY_PATH
+
+_THIS_FILE = pathlib.Path(__file__).resolve()
+_SOUFFLE_PATH = _THIS_FILE.parent / "src/souffle"
 
 @nox.session()
 def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov", *session.posargs, "tests/python")
+    session.run(POETRY_PATH, "install", external=True)
+    session.run("pytest", "--cov", *session.posargs, f"--souffle-path={_SOUFFLE_PATH}", "tests/python")
 
 # FIXME: Linting, doctest, mypy
