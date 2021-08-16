3e368eff4d341a6cb1342bff9eae8bae895b7fa1
Christian G. Warden <cwarden@xerus.org>
2021-08-16 15:16:28 -0500

    Update SWIG Interface To Support Retrieving and Updating Relations
    Add methods to SWIG interface to get relations and tuples, and to add
    tuples to relations.

--- src/include/souffle/swig/SwigInterface.h
+++ src/include/souffle/swig/SwigInterface.h
@@ -16,9 +16,159 @@
 
 #pragma once
 
+#include "souffle/RamTypes.h"
 #include "souffle/SouffleInterface.h"
 #include <iostream>
 #include <string>
+
+class SWIGSouffleTuple {
+    souffle::tuple* t;
+    souffle::Relation* relation;
+public:
+    SWIGSouffleTuple(souffle::tuple* t, souffle::Relation* r) : t(t), relation(r) {}
+
+    std::string getString(unsigned int i) {
+        (*t).rewind();
+        std::string result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return result;
+    }
+
+    void putString(std::string s) {
+        (*t) << s;
+    }
+
+#if RAM_DOMAIN_SIZE == 64
+    long long getInteger(unsigned int i) {
+        (*t).rewind();
+        souffle::RamDomain result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return (long long)result;
+    }
+
+    void putInteger(long long i) {
+            (*t) << (souffle::RamSigned)i;
+    }
+
+    unsigned long long getUnsigned(unsigned int i) {
+        (*t).rewind();
+        souffle::RamUnsigned result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return (unsigned long long)result;
+    }
+
+    void putUnsigned(unsigned long long i) {
+            (*t) << (souffle::RamUnsigned)i;
+    }
+
+    double getFloat(unsigned int i) {
+        (*t).rewind();
+        souffle::RamFloat result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return (double)result;
+    }
+
+    void putFloat(double i) {
+            (*t) << (souffle::RamFloat)i;
+    }
+
+#else
+    long getInteger(unsigned int i) {
+        (*t).rewind();
+        souffle::RamDomain result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return (long)result;
+    }
+
+    void putInteger(long i) {
+            (*t) << (souffle::RamSigned)i;
+    }
+
+    unsigned long getUnsigned(unsigned int i) {
+        (*t).rewind();
+        souffle::RamUnsigned result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return (unsigned long)result;
+    }
+
+    void putUnsigned(unsigned long i) {
+            (*t) << (souffle::RamUnsigned)i;
+    }
+
+    float getFloat(unsigned int i) {
+        (*t).rewind();
+        souffle::RamFloat result;
+        for (unsigned int j = 0; j <= i; j++) {
+            (*t) >> result;
+        }
+        return (float)result;
+    }
+
+    void putFloat(float i) {
+            (*t) << (souffle::RamFloat)i;
+    }
+
+#endif
+
+    void insert() {
+        relation->insert(*t);
+    }
+
+};
+
+class SWIGSouffleRelation {
+private:
+    souffle::Relation* relation;
+    souffle::Relation::iterator iter;
+    souffle::Relation::iterator end;
+
+public:
+    SWIGSouffleRelation(souffle::Relation* relation) : relation(relation) {
+        iter = relation->begin();
+        end = relation->end();
+    }
+
+    SWIGSouffleTuple* newTuple() {
+        return new SWIGSouffleTuple(new souffle::tuple(relation), relation);
+    }
+
+    SWIGSouffleTuple* next() {
+        SWIGSouffleTuple* t = new SWIGSouffleTuple(&(*iter), relation);
+        if (iter != end) {
+            iter++;
+        }
+        return t;
+    }
+
+    void purge() {
+        relation->purge();
+    }
+
+    std::string getSignature() {
+        return relation->getSignature();
+    }
+
+    unsigned int size() {
+        return relation->size();
+    }
+
+    unsigned int getArity() {
+        return relation->getArity();
+    }
+
+};
 
 /**
  * Abstract base class for generated Datalog programs
@@ -77,6 +227,12 @@ public:
     void dumpOutputs() {
         program->dumpOutputs();
     }
+
+    SWIGSouffleRelation* getRelation(const std::string& relationName) {
+        auto* rel = program->getRelation(relationName);
+        return new SWIGSouffleRelation(rel);
+    }
+
 };
 
 /**
@@ -87,5 +243,8 @@ public:
  */
 SWIGSouffleProgram* newInstance(const std::string& name) {
     auto* prog = souffle::ProgramFactory::newInstance(name);
+    if (!prog) {
+        return NULL;
+    }
     return new SWIGSouffleProgram(prog);
 }
