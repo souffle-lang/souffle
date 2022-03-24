203b33fe27efd96504b3220d34e32180487faef1
Langston Barrett <langston.barrett@gmail.com>
2022-03-24 11:37:11 -0400

    Try again, with renaming

--- src/ram/transform/ReorderDelta.cpp
+++ src/ram/transform/ReorderDelta.cpp
@@ -33,23 +33,40 @@ namespace souffle::ram::transform {
 
 bool ReorderDelta::reorderDelta(Program& program) {
     bool changed = false;
-    forEachQueryMap(program, [&](auto&& go, Own<Node> node) -> Own<Node> {
-        if (const Scan* scan1 = as<Scan>(node)) {
-            if (const Scan* scan2 = as<Scan>(scan1->getOperation())) {
-                const auto relation1 = scan1->getRelation();
-                const auto relation2 = scan2->getRelation();
-                const auto id1 = scan1->getTupleId();
-                const auto id2 = scan2->getTupleId();
-                if ("@delta_" + relation1 == relation2) {
-                    changed = true;
-                    auto* inner =
-                            new Scan(relation1, id2, clone(scan2->getOperation()), scan1->getProfileText());
-                    node = Own<Scan>(new Scan(relation2, id1, Own<Scan>(inner), scan2->getProfileText()));
+    forEachQuery(program, [&](Query& query) {
+        std::map<std::size_t, std::size_t> rename;
+        query.apply(nodeMapper<Node>([&](auto&& go, Own<Node> node) -> Own<Node> {
+            if (const Scan* scan1 = as<Scan>(node)) {
+                if (const Scan* scan2 = as<Scan>(scan1->getOperation())) {
+                    const auto relation1 = scan1->getRelation();
+                    const auto relation2 = scan2->getRelation();
+                    const auto id1 = scan1->getTupleId();
+                    const auto id2 = scan2->getTupleId();
+                    if ("@delta_" + relation1 == relation2) {
+                        changed = true;
+                        rename[id1] = id2;
+                        rename[id2] = id1;
+                        auto op = clone(scan2->getOperation());
+                        op->apply(nodeMapper<Node>([&](auto&& go, Own<Node> node) -> Own<Node> {
+                            if (auto* element = as<TupleElement>(node)) {
+                                if (rename[element->getTupleId()] != element->getTupleId()) {
+                                    // std::cout << "RENAMING " << *element << std::endl;
+                                    changed = true;
+                                    node = mk<TupleElement>(
+                                            rename[element->getTupleId()], element->getElement());
+                                }
+                            }
+                            node->apply(go);
+                            return node;
+                        }));
+                        auto* inner = new Scan(relation1, id2, std::move(op), scan1->getProfileText());
+                        node = Own<Scan>(new Scan(relation2, id1, Own<Scan>(inner), scan2->getProfileText()));
+                    }
                 }
             }
-        }
-        node->apply(go);
-        return node;
+            node->apply(go);
+            return node;
+        }));
     });
     return changed;
 }
