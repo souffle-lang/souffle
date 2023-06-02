30338b22367c0b3b5657be3fcb720b886290400e
Julien Henry <jhenry@mathworks.com>
2023-06-02 05:06:33 -0400

    format

--- src/interpreter/Engine.cpp
+++ src/interpreter/Engine.cpp
@@ -786,7 +786,7 @@ RamDomain Engine::execute(const Node* node, Context& ctxt) {
                     fatal("ICE: functor `%s` must map onto `NestedIntrinsicOperator`", cur.getOperator());
             }
 
-        { UNREACHABLE_BAD_CASE_ANALYSIS }
+        {UNREACHABLE_BAD_CASE_ANALYSIS}
 
 #undef BINARY_OP_LOGICAL
 #undef BINARY_OP_INTEGRAL
@@ -820,7 +820,7 @@ RamDomain Engine::execute(const Node* node, Context& ctxt) {
                 case ram::NestedIntrinsicOp::FRANGE: return RUN_RANGE(RamFloat);
             }
 
-        { UNREACHABLE_BAD_CASE_ANALYSIS }
+        {UNREACHABLE_BAD_CASE_ANALYSIS}
 #undef RUN_RANGE
         ESAC(NestedIntrinsicOperator)
 
@@ -1104,7 +1104,7 @@ RamDomain Engine::execute(const Node* node, Context& ctxt) {
                 }
             }
 
-        { UNREACHABLE_BAD_CASE_ANALYSIS }
+        {UNREACHABLE_BAD_CASE_ANALYSIS}
 
 #undef COMPARE_NUMERIC
 #undef COMPARE_STRING
