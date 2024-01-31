1f4f74928208e990e6f95a55a898e9b0a119b000
strRM <114995819+strRM@users.noreply.github.com>
2024-01-31 13:30:53 -0500

    Update check_os.sh
    On my RHEL system, ID is initially set to  'ID_LIKE="RHEL CENTOS FEDORA"', which, when expanded in the if-statement, turns into  this:
    if [ -z ID_LIKE=RHEL CENTOS FEDORA ]; then and causes the error:
    `souffle/sh/check_os.sh: line 15: [: too many arguments`
    
    It is necessary to quote $ID

--- sh/check_os.sh
+++ sh/check_os.sh
@@ -11,7 +11,7 @@ fi
 ID=$(grep -G "^ID_LIKE=" $file | tr a-z A-Z)
 
 #Fedora is special and has no ID_LIKE
-if [ -z $ID ]; then
+if [ -z "$ID" ]; then
     ID=$(grep -G "^ID=" $file | tr a-z A-Z)
 fi
 
