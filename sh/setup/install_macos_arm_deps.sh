b44660dbc66496ceb05735329f94635d7226e5e2
Quentin Sabah <quentin.sabah@gmail.com>
2024-06-03 17:14:35 +0200

    Apple ARM: use default toolchain, export SDKROOT (#2491)

--- sh/setup/install_macos_arm_deps.sh
+++ sh/setup/install_macos_arm_deps.sh
@@ -9,13 +9,14 @@ set -x
 
 # Install requirements of MAC OS X
 brew install libtool mcpp swig bison libffi
-brew install gcc@13
-brew link gcc@13
+#brew install gcc@13
+#brew link gcc@13
 
 echo "/usr/local/opt/bison/bin:$PATH" >> $GITHUB_PATH
 echo 'PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig/"' >> $GITHUB_ENV
-echo 'CC=gcc-13' >> $GITHUB_ENV
-echo 'CXX=g++-13' >> $GITHUB_ENV
+#echo 'CC=gcc-13' >> $GITHUB_ENV
+#echo 'CXX=g++-13' >> $GITHUB_ENV
+echo "SDKROOT=$(xcrun --sdk macosx --show-sdk-path)" >> $GITHUB_ENV
 
 set +e
 set +x
