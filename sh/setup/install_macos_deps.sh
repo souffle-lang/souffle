30ec732b478d70a4a41715bdacf0ce4a7149672a
Quentin Sabah <quentin.sabah@gmail.com>
2024-10-18 10:36:45 +0200

    chore: bump macos versions used in CI
    The macOS 12 runner image will be removed by December 3rd, 2024
    See https://github.com/actions/runner-images/issues/10721

--- sh/setup/install_macos_deps.sh
+++ sh/setup/install_macos_deps.sh
@@ -9,13 +9,13 @@ set -x
 
 # Install requirements of MAC OS X
 brew install bison libtool mcpp libffi swig
-brew install gcc@10
-brew link gcc@10
+brew install gcc@12
+brew link gcc@12
 
 echo "/usr/local/opt/bison/bin:$PATH" >> $GITHUB_PATH
 echo 'PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig/"' >> $GITHUB_ENV 
-echo 'CC=gcc-10' >> $GITHUB_ENV
-echo 'CXX=g++-10' >> $GITHUB_ENV
+echo 'CC=gcc-12' >> $GITHUB_ENV
+echo 'CXX=g++-12' >> $GITHUB_ENV
 
 set +e
 set +x
