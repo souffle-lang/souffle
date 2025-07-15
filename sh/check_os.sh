4a2365a9a74979884d7faf325b645bff9f4484c3
Mario Wenzel <maweki@gmail.com>
2025-07-15 10:23:44 +0200

    make mcpp optional for fedora builds. use version string to differentiate cmake behaviour.

--- sh/check_os.sh
+++ sh/check_os.sh
@@ -9,10 +9,18 @@ else
 fi
 
 ID=$(grep -G "^ID_LIKE=" $file | tr a-z A-Z)
+VERSION=$(grep -G "^VERSION_ID=" $file | tr a-z A-Z)
+
 
 #Fedora is special and has no ID_LIKE
 if [ -z "$ID" ]; then
     ID=$(grep -G "^ID=" $file | tr a-z A-Z)
 fi
 
+if [ -z "$VERSION" ]; then
+    VERSION=$(grep -G "^VERSION=" $file | tr a-z A-Z)
+fi
+
+
 echo $ID
+echo $VERSION
