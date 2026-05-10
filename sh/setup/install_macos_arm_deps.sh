ecb775c9d90ac7c12c44f4b2d73985addd36587b
Quentin Sabah <quentin.sabah@gmail.com>
2026-05-10 13:58:11 +0200

    Restore Apple ARM OpenMP CI with GCC 15

--- sh/setup/install_macos_arm_deps.sh
+++ sh/setup/install_macos_arm_deps.sh
@@ -8,15 +8,13 @@ set -e
 set -x
 
 # Install requirements of MAC OS X
-brew install libtool mcpp swig bison libffi
-#brew install gcc@13
-#brew link gcc@13
+brew install libtool mcpp swig bison libffi gcc@15
 
-echo "/usr/local/opt/bison/bin:$PATH" >> $GITHUB_PATH
-echo 'PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig/"' >> $GITHUB_ENV
-#echo 'CC=gcc-13' >> $GITHUB_ENV
-#echo 'CXX=g++-13' >> $GITHUB_ENV
-echo "SDKROOT=$(xcrun --sdk macosx --show-sdk-path)" >> $GITHUB_ENV
+echo "$(brew --prefix bison)/bin" >> "$GITHUB_PATH"
+echo "PKG_CONFIG_PATH=$(brew --prefix libffi)/lib/pkgconfig" >> "$GITHUB_ENV"
+echo "CC=$(brew --prefix gcc@15)/bin/gcc-15" >> "$GITHUB_ENV"
+echo "CXX=$(brew --prefix gcc@15)/bin/g++-15" >> "$GITHUB_ENV"
+echo "SDKROOT=$(xcrun --sdk macosx --show-sdk-path)" >> "$GITHUB_ENV"
 
 set +e
 set +x
